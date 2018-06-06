cb42acc5f0021c8b7bd900f0a93858b64080fb4c
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-06-06 01:41:40 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method heredef say_anything_x_times(phrase= "Hello", x)
-def say_hello(phrase="Hello", phrase2= "Ruby Programmer")
-  print phrase, " ",phrase2
+def say_hello(phrase2= "Ruby Programmer")
+  puts "Hello #{phrase2}"
   
 end
